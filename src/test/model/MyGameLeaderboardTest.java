fbdd877bdba5f0ecf5f93a3359af2a41baddd8ee
malcolm <malcolmteesd@Gmail.com>
2021-11-12 18:49:06 -0800

    Added testing for a method added in phase 3

--- src/test/model/MyGameLeaderboardTest.java
+++ src/test/model/MyGameLeaderboardTest.java
@@ -6,18 +6,20 @@ import persistence.JsonReader;
 import persistence.JsonWriter;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 //Tests the leaderboard class
 class MyGameLeaderboardTest {
 
-    Leaderboard l ;
+    Leaderboard l;
     Score s;
+
     @BeforeEach
     public void runBefore() {
-       l = new Leaderboard();
-       s = new Score("bob", 0);
+        l = new Leaderboard();
+        s = new Score("bob", 0);
 
     }
 
@@ -37,17 +39,17 @@ class MyGameLeaderboardTest {
     public void leaderBoardGetSizeTest() {
         // Leaderboard l = new Leaderboard();
         //Score s = new Score("bob",0);
-        assertEquals(0,l.getSize());
+        assertEquals(0, l.getSize());
 
         l.addScore(s);
-     //   assertEquals("The Leaderboard is as follows:" + "\n" + "bob 0", l.getAllScores());
+        //   assertEquals("The Leaderboard is as follows:" + "\n" + "bob 0", l.getAllScores());
         // System.out.println(l.getAllScores());
-        assertEquals(1,l.getSize());
+        assertEquals(1, l.getSize());
         l.addScore(s);
         l.addScore(s);
         l.addScore(s);
         l.addScore(s);
-        assertEquals(5,l.getSize());
+        assertEquals(5, l.getSize());
 
 
     }
@@ -55,18 +57,36 @@ class MyGameLeaderboardTest {
 
     @Test
     public void leaderBoardAddTest() {
-      //  Leaderboard l = new Leaderboard();
+        //  Leaderboard l = new Leaderboard();
         //Score s = new Score("bob", 0);
         l.addScore(s);
         assertEquals("The Leaderboard is as follows:" + "\n" + "bob 0", l.getAllScores());
-       l.addScore(new Score("bobby", 10));
-        l.addScore(new Score("obby",5));
-        assertEquals("bobby 10",l.getTopScore());
-        assertEquals("The Leaderboard is as follows:" + "\n" + "bobby 10" + "\n" + "obby 5" + "\n" + "bob 0",l.getAllScores());
-        l.addScore(new Score("fool",-5));
-        assertEquals("The Leaderboard is as follows:" + "\n" + "bobby 10" + "\n" + "obby 5" + "\n" + "bob 0" + "\n" + "fool -5",l.getAllScores());
+        l.addScore(new Score("bobby", 10));
+        l.addScore(new Score("obby", 5));
+        assertEquals("bobby 10", l.getTopScore());
+        assertEquals("The Leaderboard is as follows:" + "\n" + "bobby 10" + "\n" + "obby 5" + "\n" + "bob 0", l.getAllScores());
+        l.addScore(new Score("fool", -5));
+        assertEquals("The Leaderboard is as follows:" + "\n" + "bobby 10" + "\n" + "obby 5" + "\n" + "bob 0" + "\n" + "fool -5", l.getAllScores());
 
     }
+
+    @Test
+    public void leaderBoardGetNamesTest() {
+        //  Leaderboard l = new Leaderboard();
+        //Score s = new Score("bob", 0);
+        l.addScore(s);
+        assertEquals("The Leaderboard is as follows:" + "\n" + "bob 0", l.getAllScores());
+        l.addScore(new Score("bobby", 10));
+        l.addScore(new Score("obby", 5));
+        ArrayList<Score> names = l.getNames();
+        assertFalse(names.isEmpty());
+        for (Score s : names) {
+            System.out.println(s.getName());
+        }
+        assertEquals(l.getNamesScore(names.get(0).getName()), 1);
+    }
+
+
     @Test
     public void leaderBoardGetAllTest() {
         //Leaderboard l = new Leaderboard();
@@ -75,12 +95,13 @@ class MyGameLeaderboardTest {
         l.addScore(s);
         assertEquals("The Leaderboard is as follows:" + "\n" + "bob 0", l.getAllScores());
         l.addScore(new Score("bobby", 10));
-        l.addScore(new Score("obby",5));
-        assertEquals("The Leaderboard is as follows:" + "\n" + "bobby 10" + "\n" + "obby 5" + "\n" + "bob 0",l.getAllScores());
+        l.addScore(new Score("obby", 5));
+        assertEquals("The Leaderboard is as follows:" + "\n" + "bobby 10" + "\n" + "obby 5" + "\n" + "bob 0", l.getAllScores());
     }
+
     @Test
     public void leaderBoardGetTopTest() {
-      //  Leaderboard l = new Leaderboard();
+        //  Leaderboard l = new Leaderboard();
         //Score s = new Score("bob", 0);
         l.addScore(s);
         assertEquals("bob 0", l.getTopScore());
@@ -97,7 +118,7 @@ class MyGameLeaderboardTest {
 
     @Test
     public void leaderBoardGetNamesScoreTest() {
-      //  Leaderboard l = new Leaderboard();
+        //  Leaderboard l = new Leaderboard();
         //Score s = new Score("bob", 0);
         l.addScore(s);
         assertEquals(1, l.getNamesScore("bob"));
@@ -130,28 +151,29 @@ class MyGameLeaderboardTest {
             fail("Exception should not have been thrown");
         }
     }
+
     @Test
     void generalLeaderboardScoresToJsonTest() {
         try {
             Leaderboard lb = new Leaderboard();
-            lb.addScore(new Score( "Frisk" , 2));
-            lb.addScore(new Score( "Chara" , 13));
-            lb.addScore(new Score( "Kris" , 5));
+            lb.addScore(new Score("Frisk", 2));
+            lb.addScore(new Score("Chara", 13));
+            lb.addScore(new Score("Kris", 5));
 
             JsonWriter writer = new JsonWriter("./data/General.json");
             writer.open();
             writer.write(lb);
             writer.close();
             Leaderboard lb2 = new Leaderboard();
-            lb2.addScore(new Score( "Frisk" , 2));
-            lb2.addScore(new Score( "Chara" , 13));
-            lb2.addScore(new Score( "Kris" , 5));
+            lb2.addScore(new Score("Frisk", 2));
+            lb2.addScore(new Score("Chara", 13));
+            lb2.addScore(new Score("Kris", 5));
 
             JsonReader reader = new JsonReader("./data/General.json");
             assertTrue(lb2.toJson().getJSONArray("Scores").toString().equals(reader.read().toJson().getJSONArray("Scores").toString()));
             lb = reader.read();
 
-            assertEquals("The Leaderboard is as follows:" + "\n" + "Chara 13" + "\n" + "Kris 5" + "\n" + "Frisk 2",lb.getAllScores());
+            assertEquals("The Leaderboard is as follows:" + "\n" + "Chara 13" + "\n" + "Kris 5" + "\n" + "Frisk 2", lb.getAllScores());
 
 
         } catch (IOException e) {
